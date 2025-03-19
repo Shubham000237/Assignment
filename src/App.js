f28f628513f9372c98348725083ecce44de338b1
Shubham000237 <sampardeshi30@gmail.com>
2025-03-19 12:32:12 +0530

    Main route file added

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {SignUp, Login, Dashboard, Todo, StopWatch} from '../src/Screens';
+import { ProtectedRouting } from '../src/Route/ProtectedRouting/ProtectedRouting';
+import { PublicRouting } from '../src/Route/PublicRouting/PublicRouting';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route element={<PublicRouting />}>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+
+        <Route element={<ProtectedRouting />}>
+        <Route path="/home" element={<Dashboard />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/stopwatch" element={<StopWatch />} />
+        </Route>
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
